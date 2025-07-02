bd58ba15d3cd2b5d4cfe93cb79d58563c993c12d
thanhhuynh1903 <thanhgaming1234@gmail.com>
2025-07-02 12:10:00 +0700

    add scroll wheel

--- src/components/scrollreel.tsx
+++ src/components/scrollreel.tsx
@@ -3,11 +3,11 @@ import { ChevronUp, ChevronDown } from 'lucide-react'
 export default function Scrollreel() {
     return (
         <div className='content-center px-4 space-y-4'>
-            <div className="content-center rounded-full transform bg-gray-800/80 backdrop-blur-md p-4 space-y-4">
-                <ChevronUp className="w-4 h-4 text-white animate-bounce" />
+            <div className="content-center rounded-full transform bg-gray-800/80 backdrop-blur-md p-4 space-y-4 animate-bounce">
+                <ChevronUp className="w-4 h-4 text-white " />
             </div>
-            <div className="content-center rounded-full transform bg-gray-800/80 backdrop-blur-md p-4 space-y-4">
-                <ChevronDown className="w-4 h-4 text-white animate-bounce" />
+            <div className="content-center rounded-full transform bg-gray-800/80 backdrop-blur-md p-4 space-y-4 animate-bounce">
+                <ChevronDown className="w-4 h-4 text-white " />
             </div>
         </div>
     )
